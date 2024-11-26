9f17868528003c3d4a4db921d51b69609c788268
Amani <amanialshmali7@gmail.com>
2024-11-26 16:21:19 +0400

    adding ITransactionRepository

---
+++ Repositories/ITransactionRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServicesLab1.Models;
+
+
+namespace ServicesLab1.Repositories
+{
+    public interface ITransactionRepository
+    {
+        IEnumerable<Transaction> GetAllTransactions();
+
+        Transaction GetTransactionById(int transactionId);
+
+        void AddTransaction(Transaction transaction);
+
+        void UpdateTransaction(Transaction transaction);
+
+        void DeleteTransaction(int transactionId);
+
+   
+
+
+
+    }
+}
